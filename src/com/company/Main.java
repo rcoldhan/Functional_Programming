a0cee29c63532a64a74106daa9f7aa3df558643e
Yulia Kozlova <rcoldhan@gmail.com>
2021-03-24 18:07:16 +0300

    final

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,23 +1,53 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-        int input = <получить число из ввода>
-        массив1 = getFilledArray(10, input, лямбда);
-        Вывод_в_консоль(массив1_строкой);
-        массив2 = getFilledArray(10, input, лямбда);
-        Вывод_в_консоль(массив2_строкой);
-        ...
-        массивN = getFilledArray(10, input, лямбда);
-        Вывод_в_консоль(массивN_строкой);
-        scanner.close()
+    public static void main(final String[] args) {
+        Scanner console = new Scanner(System.in);
+        int arrSize = 10;
+
+        int userInput = console.nextInt();
+
+        //index + mod
+        int[] arrA = getFilledArray(arrSize, userInput, Integer::sum);
+        System.out.println("A: " + Arrays.toString(arrA));
+
+        //index * mod
+        int[] arrB = getFilledArray(arrSize, userInput, (index, mod) -> index * mod);
+        System.out.println("B: " + Arrays.toString(arrB));
+
+        /*
+         * for even index: (index / 2) + mod
+         * for odd index:  (index * 2) - mod
+         */
+        int[] arrC = getFilledArray(arrSize, userInput, (index, mod) -> {
+            if (index % 2 == 0) {
+                return index / 2 + mod;
+            } else {
+                return (int) Math.pow(index, 2) - mod;
+            }
+        });
+        System.out.println("C: " + Arrays.toString(arrC));
+
+        //mod - index
+        int[] arrD = getFilledArray(arrSize, userInput, ((index, mod) -> mod - index));
+        System.out.println("D: " + Arrays.toString(arrD));
+
+        //index + (mod * 2)
+        int[] arrE = getFilledArray(arrSize, userInput, ((index, mod) -> index + (mod * 2)));
+        System.out.println("E: " + Arrays.toString(arrE));
+
+        console.close();
     }
 
-    getFilledArray(размер, модификатор, функц_интерфейс) {
-        <создание и заполнение массива с использованием метода process>
-        return массив;
-    }
-}
+    public static int[] getFilledArray(final int arrSize, final int userInput, final Process p) {
+        int[] filledArr = new int[arrSize];
+        for (int i = 0; i < filledArr.length; i++) {
+            filledArr[i] = p.process(i, userInput);
+        }
+        return filledArr;
     }
 }
---
+++ src/com/company/Process.java
@@ -0,0 +1,6 @@
+package com.company;
+
+@FunctionalInterface
+interface Process {
+    int process(int index, int mod);
+}
